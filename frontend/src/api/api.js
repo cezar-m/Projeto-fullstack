ba807aab7c4affb52238ff33a854785d1669798b
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-01-23 03:27:40 -0300

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-console.log("API =", import.meta.env.REACT_APP_API_URL);
+console.log("API =", process.env.REACT_APP_API_URL);
 
 const api = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true
 });
 
