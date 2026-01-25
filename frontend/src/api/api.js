81faa28067b02d14740bb8e00d30b8675cf33077
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-01-25 02:17:02 -0300

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -5,8 +5,7 @@ const BASE_URL = process.env.REACT_APP_API_URL;
 if (!BASE_URL) console.error("⚠️ REACT_APP_API_URL não está definido!");
 
 const api = axios.create({
-  baseURL: BASE_URL + "/api",
-  withCredentials: true
+  baseURL: BASE_URL + "/api
 });
 
 export default api;
