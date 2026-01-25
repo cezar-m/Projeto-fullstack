b6f692b1cec2d5cc73f0aa39e4f562b5049ab56d
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-01-24 21:57:38 -0300

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,24 +1,21 @@
+// middleware/authMiddleware.js
 import jwt from "jsonwebtoken";
 
 export const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
-
   if (!authHeader) return res.status(401).json({ message: "Token não fornecido" });
 
   const token = authHeader.split(" ")[1];
-
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET || "SECRET_TEMP");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // id + role
     next();
-  } catch (err) {
+  } catch {
     return res.status(401).json({ message: "Token inválido" });
   }
 };
 
 export const isAdmin = (req, res, next) => {
-  if (req.user.role !== "admin") {
-    return res.status(403).json({ message: "Acesso negado: apenas admin" });
-  }
+  if (req.user.role !== "admin") return res.status(403).json({ message: "Acesso negado: apenas admin" });
   next();
 };
