bddc571071697560a8371fd6a3d61a0813b2ba13
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-01-25 23:23:41 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,13 @@
+// db.js
 import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
 const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-// 🔥 FORÇA O SCHEMA NO MOMENTO DA CONEXÃO
-pool.on("connect", async (client) => {
-  await client.query("SET search_path TO sistema_admin");
-  console.log("✅ Schema definido como sistema_admin");
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
