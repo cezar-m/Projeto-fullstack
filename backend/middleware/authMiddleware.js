9ef5b87d0eee6ab655a5350d0a2042633fb43f6a
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-01-26 22:51:51 -0300

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -21,7 +21,7 @@ export const authMiddleware = (req, res, next) => {
 };
 
 export const isAdmin = (req, res, next) => {
-  if (!req.user || req.user.role !== "admin") {
+  if (!req.user?.role || req.user.role.toLowerCase() !== "admin") {
     return res.status(403).json({ message: "Acesso negado: apenas admin" });
   }
   next();
