3b80d9770fe174ca073d57c637e2c3f59c1153ef
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-12 03:46:36 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,21 +5,14 @@ dotenv.config();
 const { Pool } = pkg;
 
 const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // ⚡ obrigatório no Render
-  max: 5, // limite de conexões
-});
-
-// Reconectar automaticamente se a conexão cair
-db.on("error", async (err) => {
-  console.error("❌ Erro de conexão com o banco:", err.message);
-  try {
-    await db.connect();
-    console.log("🔄 Reconectado ao banco!");
-  } catch (e) {
-    console.error("❌ Falha ao reconectar:", e.message);
-  }
+  host: "aws-1-us-east-2.pooler.supabase.com",
+  port: 6543,
+  user: "postgres.pivokxepxqugqwpkyxlw",
+  password: "QMiYG7oSeoFqUmbH",
+  database: "postgres",
+  ssl: { rejectUnauthorized: false }
 });
 
 export default db;
+
 
