e214a2e2b2b8886722172bd10e315f87ba93775d
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-12 02:20:15 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,18 @@
 import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const { Pool } = pkg;
 
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  max: 5,
 });
 
-export default db; // ✅ exportação default
+db.on("error", (err) => {
+  console.error("Erro de conexão com o banco:", err);
+});
+
+export default db;
