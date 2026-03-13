293ecf4d5b0bd8d570df28e77941bab1bbd7accd
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-13 01:18:55 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,15 +2,14 @@ import pkg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 const { Pool } = pkg;
 
 const db = new Pool({
-  host: "aws-1-us-east-2.pooler.supabase.com",
-  port: 6543,
-  user: "postgres.pivokxepxqugqwpkyxlw",
-  password: "QMiYG7oSeoFqUmbH",
-  database: "postgres",
-  ssl: { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default db;
