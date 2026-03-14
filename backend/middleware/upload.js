4b18dbb86d3369b88d908146ef75373853b4e40e
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-14 01:58:25 -0300

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -11,14 +11,10 @@ export const uploadToCloudinary = (fileBuffer, folder = "produtos") =>
   new Promise((resolve, reject) => {
     if (!fileBuffer) return reject(new Error("Arquivo inválido"));
 
-    const stream = cloudinary.uploader.upload_stream(
-      { folder },
-      (error, result) => {
-        if (error) return reject(error);
-        resolve(result);
-      }
-    );
+    const stream = cloudinary.uploader.upload_stream({ folder }, (error, result) => {
+      if (error) return reject(error);
+      resolve(result);
+    });
 
     streamifier.createReadStream(fileBuffer).pipe(stream);
   });
-
