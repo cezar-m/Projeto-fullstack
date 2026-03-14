46a89ce4843305ec1eb2738f8ecee5d964726423
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-14 02:07:18 -0300

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,3 +1,4 @@
+import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
 import streamifier from "streamifier";
 
@@ -6,6 +7,8 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+export const upload = multer({ storage: multer.memoryStorage() });
 
 export const uploadToCloudinary = (fileBuffer, folder = "produtos") =>
   new Promise((resolve, reject) => {
